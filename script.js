420691bca109e3623ff5985ff7937fd5c4826b66
Jayalakshmi A <jayalakshmi1407a@gmail.com>
2025-02-21 15:17:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+        const buttons = document.querySelectorAll('.product button');
+        buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                alert('Product added to cart!');
+            });
+        });
+    
